d56be98b57c162a06581e19aef9e08f726eecff2
nhat <puddinsun@gmail.com>
2023-03-16 14:08:10 +0700

    remove login filter

--- src/utils/Store.ts
+++ src/utils/Store.ts
@@ -1,43 +1,41 @@
-import { ReviewRequestExtend } from "../models/review.model"
+import { ReviewRequestExtend } from "../models/review.model";
 
 class Store {
-    constructor() { }
-    getListReviews() {
-        if (localStorage.getItem("isLogin")?.length) {
-            return JSON.parse(localStorage.getItem('listReviews') || '[]').reverse()
-        }
-        return [];
-    }
-    addReview(review: ReviewRequestExtend) {
-        const listReviews = this.getListReviews()
-        listReviews.push(review)
-        localStorage.setItem("listReviews", JSON.stringify(listReviews))
-    }
-    editReview(review: any) {
-        const listReviews = this.getListReviews()
-        const listReviewsAfterEdit = listReviews.map((reviewItem: any) => {
-            if (reviewItem.id === review.id) {
-                return review
-            }
-            return reviewItem
-        })
-        localStorage.setItem("listReviews", JSON.stringify(listReviewsAfterEdit))
-
-    }
-    getDraft() {
-        const listReviews: ReviewRequestExtend[] = this.getListReviews()
-        return listReviews.find((item: ReviewRequestExtend) => item.isDraft)
-    }
-    getUser() {
-        const userName = localStorage.getItem("isLogin") || ''
-        return userName
-    }
-    getMyReview() {
-        const listReviews: ReviewRequestExtend[] = this.getListReviews()
-        const currentUser = this.getUser()
-        return listReviews.filter((item: ReviewRequestExtend) => item.authorId === currentUser)
-
-    }
+  constructor() {}
+  getListReviews() {
+    return JSON.parse(localStorage.getItem("listReviews") || "[]").reverse();
+    return [];
+  }
+  addReview(review: ReviewRequestExtend) {
+    const listReviews = this.getListReviews();
+    listReviews.push(review);
+    localStorage.setItem("listReviews", JSON.stringify(listReviews));
+  }
+  editReview(review: any) {
+    const listReviews = this.getListReviews();
+    const listReviewsAfterEdit = listReviews.map((reviewItem: any) => {
+      if (reviewItem.id === review.id) {
+        return review;
+      }
+      return reviewItem;
+    });
+    localStorage.setItem("listReviews", JSON.stringify(listReviewsAfterEdit));
+  }
+  getDraft() {
+    const listReviews: ReviewRequestExtend[] = this.getListReviews();
+    return listReviews.find((item: ReviewRequestExtend) => item.isDraft);
+  }
+  getUser() {
+    const userName = localStorage.getItem("isLogin") || "";
+    return userName;
+  }
+  getMyReview() {
+    const listReviews: ReviewRequestExtend[] = this.getListReviews();
+    const currentUser = this.getUser();
+    return listReviews.filter(
+      (item: ReviewRequestExtend) => item.authorId === currentUser
+    );
+  }
 }
-const store = new Store()
-export default store
+const store = new Store();
+export default store;
